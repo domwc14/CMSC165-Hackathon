91b5ba1f49b8bc4ec4d39a396e392d3032b67da8
alaingranada <atgranada@up.edu.ph>
2022-12-12 21:05:14 +0800

    Update gui.py
    trackbars added

--- gui.py
+++ gui.py
@@ -7,6 +7,10 @@ import numpy as np
 
 global text 
 text = "Pollinated"
+
+def nothing(x):
+    pass
+
 def select_image():
 
     path = filedialog.askopenfilename()
@@ -61,7 +65,7 @@ def detect_image(): #function to display altered image
         label_img = tk.Label(app, image=image, relief=tk.SOLID)
         label_img.image = image 
         label_img.place(x=1200, y=150)
-
+        sample_text.insert(0, "Detected: ")
 
 app = tk.Tk()
 
@@ -70,73 +74,37 @@ height = app.winfo_screenheight()
 
 app.geometry('{}x{}'.format(width, height))
 
+
+#(win, text="Button-1",height= 5, width=10).pack()
 btn = tk.Button(app, text="Select Image", command=select_image, width=12)
 btn.place(x=150, y=600)
 
 btn = tk.Button(app, text="Detect Image", command=detect_image, width=12)
 btn.place(x=150, y=640)
 
-label1 = tk.Label(app, text='HSV', width=7, height=2)
-label1.place(x=50, y=160)
-
-hsv= tk.StringVar()
-w1 = tk.Scale(app, from_=0, to=1, orient=tk.HORIZONTAL, variable=hsv, width=15, command=lambda value:modify_image('h_min', value))
-w1.place(x=150, y=150)
-
-label2 = tk.Label(app, text='L-H', width=7, height=2)
-label2.place(x=50, y=210)
-
-l_h= tk.StringVar()
-w2 = tk.Scale(app, from_=0, to=255, orient=tk.HORIZONTAL, variable=l_h, width=15, command=lambda value:modify_image('s_min', value))
-w2.place(x=150, y=200)
+#textbox below the buttons
+sample_text = tk.Entry(app)
+sample_text.place(x=150, y=690)
+#sample_text.config(state=DISABLED)
 
 
-label3 = tk.Label(app, text='L-S', width=7, height=2)
-label3.place(x=50, y=260)
+window_name = "Trackbars"
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+cv2.setWindowProperty(window_name, cv2.WND_PROP_TOPMOST, 1)
+cv2.resizeWindow(window_name,300,40) #w,h
+cv2.createTrackbar("L - H", window_name, 0, 179, nothing)
+cv2.createTrackbar("L - S", window_name, 0, 255, nothing)
+cv2.createTrackbar("L - V", window_name, 0, 255, nothing)
+cv2.createTrackbar("U - H", window_name, 179, 179, nothing)
+cv2.createTrackbar("U - S", window_name, 255, 255, nothing)
+cv2.createTrackbar("U - V", window_name, 255, 255, nothing)
 
-l_s= tk.StringVar()
-w3 = tk.Scale(app, from_=0, to=255, orient=tk.HORIZONTAL, variable=l_s, width=15, command=lambda value:modify_image('v_min', value))
-w3.place(x=150, y=250)
-
-label4 = tk.Label(app, text='L-V', width=7, height=2)
-label4.place(x=50, y=310)
-
-l_v= tk.StringVar()
-w4 = tk.Scale(app, from_=0, to=255, orient=tk.HORIZONTAL, variable=l_v, width=15, command=lambda value:modify_image('v_min', value))
-w4.place(x=150, y=300)
-
-label5 = tk.Label(app, text='U-H', width=7, height=2)
-label5.place(x=50, y=360)
-
-u_h= tk.StringVar()
-w5 = tk.Scale(app, from_=0, to=255, orient=tk.HORIZONTAL, variable=u_h, width=15, command=lambda value:modify_image('v_min', value))
-w5.place(x=150, y=350)
-
-
-label6 = tk.Label(app, text='U-S', width=7, height=2)
-label6.place(x=50, y=410)
-
-u_s= tk.StringVar()
-w6 = tk.Scale(app, from_=0, to=255, orient=tk.HORIZONTAL, variable=u_s, width=15, command=lambda value:modify_image('v_min', value))
-w6.place(x=150, y=400)
-
-label7 = tk.Label(app, text='U-V', width=7, height=2)
-label7.place(x=50, y=460)
-
-u_v= tk.StringVar()
-w7 = tk.Scale(app, from_=0, to=255, orient=tk.HORIZONTAL, variable=u_v, width=15, command=lambda value:modify_image('v_min', value))
-w7.place(x=150, y=450)
-
-label8 = tk.Label(app, text='Gaussian Blur', width=20, height=2)
-label8.place(x=10, y=510)
-
-g_blur= tk.StringVar()
-w8 = tk.Scale(app, from_=0, to=1, orient=tk.HORIZONTAL, variable=g_blur, width=15, command=lambda value:modify_image('v_min', value))
-w8.place(x=150, y=500)
-
-
-
-label9 = tk.Label(app, text=text, width=7, height=2)
-label9.place(x=50, y=700)
+# while True:
+#     l_h = cv2.getTrackbarPos("L - H", window_name)
+#     l_s = cv2.getTrackbarPos("L - S", window_name)
+#     l_v = cv2.getTrackbarPos("L - V", window_name)
+#     u_h = cv2.getTrackbarPos("U - H", window_name)
+#     u_s = cv2.getTrackbarPos("U - S", window_name)
+#     u_v = cv2.getTrackbarPos("U - V", window_name)
 
 app.mainloop()
